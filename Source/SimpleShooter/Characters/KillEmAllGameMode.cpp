c44a3aee7d8c7aa90094cae9e91949e45280e099
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2021-04-25 22:23:37 +0530

    added iteration over actors

--- Source/SimpleShooter/Characters/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/Characters/KillEmAllGameMode.cpp
@@ -1,6 +1,8 @@
 // Sudip Mondal
 
 #include "KillEmAllGameMode.h"
+#include "EngineUtils.h"
+#include "GameFramework/Controller.h"
 
 void AKillEmAllGameMode::PawnKilled(APawn *PawnKilled)
 {
@@ -9,6 +11,15 @@ void AKillEmAllGameMode::PawnKilled(APawn *PawnKilled)
     APlayerController *PlayerController = Cast<APlayerController>(PawnKilled->GetController());
     if (PlayerController != nullptr)
     {
-        PlayerController->GameHasEnded(nullptr, false);
+        EndGame(false);
+    }
+}
+
+void AKillEmAllGameMode::EndGame(bool bIsPlayerWinner) 
+{
+    for (AController* Controller : TActorRange<AController>(GetWorld()))
+    {
+        bool bIsWinner = Controller->IsPlayerController() == bIsPlayerWinner;
+        Controller->GameHasEnded(Controller->GetPawn(), bIsWinner);
     }
 }
--- Source/SimpleShooter/Characters/KillEmAllGameMode.h
+++ Source/SimpleShooter/Characters/KillEmAllGameMode.h
@@ -16,6 +16,9 @@ class SIMPLESHOOTER_API AKillEmAllGameMode : public ASimpleShooterGameModeBase
 	
 public:
 	virtual void PawnKilled(APawn* PawnKilled) override;
+	
+private:
+	void EndGame(bool bIsPlayerWinner);
 
 };
 
